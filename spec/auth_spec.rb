1532c78175f8b9f26d5967c58628fd02b732e6e3
jriby <julien.riby@etu.enseeiht.fr>
2012-03-07 00:57:07 +0100

    Ajout de get et post sauth/register dans sauth.rb avec ses tests et ses views

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -1,5 +1,6 @@
-require '../sauth'  # <-- your sinatra app
+require '../sauth'
 require 'rack/test'
+require 'test/unit'
 
 set :environment, :test
 
@@ -18,9 +19,44 @@ describe 'The sauth App' do
     end
     
     it "should return status 200 if a user go to /sauth/register" do
+      get '/sauth/register'   
       last_response.status.should == 200
     end
+  
 
- 
+      context "The login and the pass are ok" do
+
+	it "should create the person with login and pass in the bdd " do
+          post '/sauth/register', :login => 'jriby', :passwd => 'pass'
+          last_response.status.should == 302
+          last_response.headers["Location"].should == "http://example.org/sauth/registerok"
+          Person.all.each{|p| p.destroy}
+        end
+      end
+      context "The login and the pass are not ok" do
+	it "should redirect at /sauth/register if the login are ever used" do
+          p=Person.new()
+	  p.login = "login"
+	  p.passwd = "pass"	
+          p.save
+          post '/sauth/register', :login => 'login', :passwd => 'coucou'
+          last_response.status.should == 302
+          last_response.headers["Location"].should == "http://example.org/sauth/register?error=Login_deja_pris"
+	  #p1.destroy
+        end
+	it "should redirect at /sauth/register if the pass is empty" do
+          post '/sauth/register', :login => 'lol', :passwd => ''
+          last_response.status.should == 302
+          last_response.headers["Location"].should == "http://example.org/sauth/register?error=Veuillez_entrer_un_mot_de_passe"
+        end
+	it "should redirect at /sauth/register if the login empty" do
+          post '/sauth/register', :login => '', :passwd => 'lol'
+          last_response.status.should == 302
+          last_response.headers["Location"].should == "http://example.org/sauth/register?error=Veuillez_entrer_un_login"
+        end
+
+
+      end
+  end 
 end
 
