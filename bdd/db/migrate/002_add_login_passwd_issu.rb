2c1a41b70e1dc1aec94b971e3236c99df292c339
jriby <julien.riby@etu.enseeiht.fr>
2012-03-07 17:57:47 +0100

    Ajout de la table applications et ajout de is_su dans user

--- bdd/db/migrate/002_add_login_passwd_issu.rb
+++ bdd/db/migrate/002_add_login_passwd_issu.rb
@@ -1,7 +1,8 @@
-class AddLoginPasswd < ActiveRecord::Migration
+class AddLoginPasswdIssu < ActiveRecord::Migration
   def up
     add_column :users, :login, :string
     add_column :users, :passwd, :string
+    add_column :users, :is_super_user, :boolean, :default => false
   end
 
   def down
