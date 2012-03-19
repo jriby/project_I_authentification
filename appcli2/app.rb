0fdc106bc853958ed8929e0d328fd8a23a29de4e
jriby <julien.riby@etu.enseeiht.fr>
2012-03-20 00:54:09 +0100

    Ajout de l'appcli2

---
+++ appcli2/app.rb
@@ -0,0 +1,47 @@
+$: << File.dirname(__FILE__)
+
+require 'sinatra'
+
+set :port, 6002
+
+enable :sessions
+
+helpers do
+  def current_user
+    session["current_user_app2"]
+  end
+end
+
+
+#########################
+# Index
+#########################
+get '/' do
+  erb :index
+end
+
+
+#########################
+# Protected
+#########################
+get '/protected' do
+
+  if current_user
+    #j'ai un cookie dans app
+    erb :protected
+
+  #j'ai pas de cookie ds app
+  elsif params['secret'].nil?
+    #Pas de secret donc ce n'est pas une rep de sauth
+    redirect 'http://localhost:6666/app2/session/new?origin=/protected'
+    elsif params['secret']=="jesuisauth" && !params['login'].nil?
+      #L'auth est OK
+      session["current_user_app2"] = params['login']
+      erb :protected
+      
+      else
+        #Pas de secret ou de param login -> je ne parle pas ac le bon sauth
+        'Probleme avec le sauth !'
+   end
+end
+
---
+++ appcli2/spec/app_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+require 'uri'
+describe 'app' do
+
+#########################
+#get /
+#########################
+  describe "get /" do
+      
+    it "should get /" do
+      get '/'
+      last_response.should be_ok
+      last_request.path.should == '/'
+    end
+
+    it "should return the index page" do
+      get '/'
+      last_response.body.should match %r{<h1>App cli 2</h1>.*}
+     end
+  end
+
+#########################
+#get /protected
+#########################
+  describe "get /protected" do
+    
+    context "Without session" do
+      it "should redirect to the sauth" do      
+          get '/protected'
+          last_response.should be_redirect
+          follow_redirect!
+          last_request.url.should == 'http://localhost:6666/app2/session/new?origin=%2Fprotected'
+      end
+    end
+    context "Reponse du sauth" do
+    before do
+              @params = { 'secret' => "jesuisauth", "login" => "log"}
+    end
+      it "should redirect to protected" do      
+          get '/protected', @params
+          last_response.body.should match %r{<h1>Page privee appli cli 2</h1>.*}
+      end
+      it "should set the session" do      
+          get '/protected', @params
+          last_request.env["rack.session"]["current_user_app2"].should == "log"
+      end
+    end
+
+    context "With session" do
+    before do
+      @params = { 'secret' => "jesuisauth", "login" => "log"}
+      get '/protected', @params
+      last_request.env["rack.session"]["current_user_app2"].should == "log"
+    end
+
+      it "should get /protected" do
+        get '/protected'
+        last_response.should be_ok
+        last_request.path.should == '/protected'
+      end
+
+      it "should return the private page" do
+        get '/protected'
+        last_response.body.should match %r{<h1>Page privee appli cli 2</h1>.*}
+      end
+    end
+
+    context "Bad secret" do
+      it "should have the message Probleme avec le sauth !" do      
+        @params = { 'secret' => "jenesuispasauth", "login" => "log"}
+        get '/protected', @params
+        last_response.body.should match %r{Probleme avec le sauth !}
+      end
+    end
+
+  end
+end
